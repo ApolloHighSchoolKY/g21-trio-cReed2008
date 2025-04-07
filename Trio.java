cd7d58106eb4594973bf4193adcac5e4db33488a
cReed2008 <colby.reed@stu.daviess.kyschools.us>
2025-04-07 14:27:27 +0000

    Done sorry its late

--- Trio.java
+++ Trio.java
@@ -1,1 +1,26 @@
 //Write your answer here
+public class Trio implements MenuItem
+{
+    private Sandwich sandy;
+    private Salad sally;
+    private Drink bev;
+
+    public Trio( Sandwich san, Salad sal, Drink dri)
+    {
+        sandy = san;
+        sally = sal;
+        bev = dri;
+    }
+
+    public String getName()
+    {
+        return sandy.getName() + "/" + sally.getName() + "/" + bev.getName() + " Trio";
+    }
+
+    public double getPrice()
+    {
+        double sandyPrice = sandy.getPrice();
+        double sallyPrice = sally.getPrice();
+        double bevPrice = bev.getPrice();
+    }
+}
